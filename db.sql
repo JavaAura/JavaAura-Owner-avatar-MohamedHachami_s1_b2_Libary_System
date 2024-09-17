d2b69a99c33bbd4a1561353ff42fe7959a4664b2
MedHachami <moha.hach.9@gmail.com>
2024-09-17 04:27:37 +0100

    table borrowHistory and reservation

--- db.sql
+++ db.sql
@@ -1,56 +1,4 @@
 
-
--- CREATE TABLE Document (
---     id SERIAL PRIMARY KEY,
---     titre VARCHAR(255) NOT NULL,
---     auteur VARCHAR(255) NOT NULL,
---     datePublication DATE NOT NULL,
---     nombreDePages BIGINT NOT NULL,
---     type VARCHAR(20) NOT NULL,
---     available INT NOT NULL
--- );
-
-
--- CREATE TABLE Livre (
---     id SERIAL PRIMARY KEY,
---     document_id INT UNIQUE, 
---     isbn VARCHAR(13) NOT NULL UNIQUE,
---     CONSTRAINT FK_Livre_Document FOREIGN KEY (document_id)
---         REFERENCES Document(id) ON DELETE CASCADE
--- ) INHERITS (Document);
-
-
--- CREATE TABLE Magazine (
---     id SERIAL PRIMARY KEY,
---     document_id INT UNIQUE, 
---     numero BIGINT NOT NULL UNIQUE,
---     CONSTRAINT FK_Magazine_Document FOREIGN KEY (document_id)
---         REFERENCES Document(id) ON DELETE CASCADE
--- ) INHERITS (Document);
-
--- CREATE TABLE JournalScientifique (
---     id SERIAL PRIMARY KEY,
---     document_id INT UNIQUE,
---     domaineRecherche VARCHAR(255) NOT NULL,
---     CONSTRAINT FK_JournalScientifique_Document FOREIGN KEY (document_id)
---         REFERENCES Document(id) ON DELETE CASCADE
--- ) INHERITS (Document);
-
--- CREATE TABLE ThèseUniversitaire (
---     id SERIAL PRIMARY KEY,
---     document_id INT UNIQUE,
---     université VARCHAR(255) NOT NULL,
---     domaine VARCHAR(255) NOT NULL,
---     CONSTRAINT FK_ThèseUniversitaire_Document FOREIGN KEY (document_id)
---         REFERENCES Document(id) ON DELETE CASCADE
--- ) INHERITS (Document);
-
-
--- connect ->  sudo -u postgres psq
--- show databases  -> \l
---- connect to db -> \c booktrack 
-
-
     CREATE TABLE Document (
         id  SERIAL PRIMARY KEY,
         titre VARCHAR(255) NOT NULL,
@@ -58,7 +6,7 @@
         datePublication DATE NOT NULL,
         nombreDePages BIGINT NOT NULL,
         type VARCHAR(20) NOT NULL,
-        available BOOLEAN DEFAULT TRUE
+        available int NOT NULL 
     );
 
 
@@ -109,7 +57,7 @@ CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE,
     email VARCHAR(255) NOT NULL,
-    type_user VARCHAR(50) CHECK (type_user IN ('Professor', 'Student','Admin')) NOT NULL
+    type_user VARCHAR(50) CHECK (type_user IN ('Professor', 'Student')) NOT NULL
 ) ;
 
 CREATE TABLE Student (
@@ -122,19 +70,10 @@ CREATE TABLE Professor (
 ) INHERITS (Users);
 
 
-
-INSERT INTO Student (name, email, type_user, niveau_etude)
-VALUES ('Alice Dupont', 'alice.dupont@example.com', 'Student', 'Master Informatique');
-
-
-
-INSERT INTO Professor (name, email, type_user, specialite)
-VALUES ('Khalil', 'Khalil.mr@example.com', 'Professor', ' Informatique');
-
 CREATE TABLE BorrowHistory (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,  -- Assuming Users.id is INTEGER
-    document_id INTEGER NOT NULL,  -- Assuming Document.id is INTEGER
+    user_id INTEGER NOT NULL, 
+    document_id INTEGER NOT NULL,  
     borrow_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     return_date TIMESTAMP,
     CONSTRAINT FK_DOC_BORROW FOREIGN KEY (document_id)
@@ -143,26 +82,9 @@ CREATE TABLE BorrowHistory (
         REFERENCES Users(id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE reservation(
+CREATE TABLE reservation (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES Users(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
     document_id INTEGER NOT NULL REFERENCES Document(id),
     date_reservation DATE NOT NULL
 );
-
-
-
-module com.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires javafx.graphics;
-    
-    requires io.github.cdimascio.dotenv;
-    opens io.github.cdimascio.dotenv;
-
-    opens com.example.controllers to javafx.fxml;
-    opens com.example.Model to javafx.base;
-    exports com.example;
-}
